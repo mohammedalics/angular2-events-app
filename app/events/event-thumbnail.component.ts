7d51e6e7245ee8ee3cf20b12a775e8fbbd14d32c
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-04-28 22:21:02 +0200

    Add class using angular

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -5,6 +5,8 @@ import { Component , Input , Output, EventEmitter } from '@angular/core'
         selector: 'event-thumbnail', 
         templateUrl: '/app/events/event-thumbnail.component.html', 
         styles: [`
+            .green { color: green; !important }
+            .bold { font-weight: bold; !important }
             .thumbnail { min-height: 210px; } 
             .pad-left { margin-left: 10px; }
             .well dev { color: #bbb; }
