ea7564d5ff63a132db1dc5b554e265703cfccd7a
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-04-30 00:49:05 +0200

    set the return of the getEvents service in timeout method (observable) to simulate the http response delay and see the page partially loading

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -8,13 +8,13 @@ import { ToastrService } from '../common/toastr.service'
     }
 )
 export class EventsListComponent implements OnInit {
-  events: any[]
+  events: any
   constructor(private eventService : EventService, private toastrService : ToastrService) {
     
   }
 
   ngOnInit() {
-    this.events = this.eventService.getEvents()
+    this.eventService.getEvents().subscribe(events => {this.events = events;}); 
   }
   
   handleThumbnailClick(eventName) {
