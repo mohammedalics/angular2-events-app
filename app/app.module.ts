51bb724221c4f8b62e427540de2c3c10c3527770
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-04-29 12:04:20 +0200

    Add event service

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,12 @@ import { EventAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
 
 @NgModule({
     imports: [BrowserModule], 
     declarations: [EventAppComponent, EventsListComponent, EventThumbnailComponent, NavBarComponent], 
+    providers: [EventService],
     bootstrap: [EventAppComponent]
 })
 export class AppModule {}
--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component , Input , Output, EventEmitter } from '@angular/core'
+import { Component , Input } from '@angular/core'
 
 @Component(
     {
@@ -14,11 +14,10 @@ import { Component , Input , Output, EventEmitter } from '@angular/core'
     }
 )
 export class EventThumbnailComponent {
-        @Input() event: any
+    @Input() event: any
 
-        getStartTimeClass() {
-            const isEarlyStart = this.event && this.event.time === '8:00 am'
-            return { green: isEarlyStart, bold: isEarlyStart }
-        }
+    getStartTimeClass() {
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+        return { green: isEarlyStart, bold: isEarlyStart }
     }
 }
