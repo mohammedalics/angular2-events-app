12fadc21c440e96997c44acef5fc96b21b86ccdd
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-04-29 18:00:13 +0200

    Add error page and event-route-activator

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,10 +6,12 @@ import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { CreateEventComponent } from './events/create-event/create-event.component'
+import { Error404Component } from './errors/404.component'
 
 import { NavBarComponent } from './nav/navbar.component'
 import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
+import { EventRouterActivator } from './events/event-route-activator.service'
 import { appRoutes } from './routes'
 
 @NgModule({
@@ -23,9 +25,10 @@ import { appRoutes } from './routes'
         EventThumbnailComponent, 
         EventDetailsComponent, 
         CreateEventComponent, 
+        Error404Component,
         NavBarComponent
         ], 
-    providers: [EventService, ToastrService],
+    providers: [EventService, ToastrService, EventRouterActivator],
     bootstrap: [EventAppComponent]
 })
 export class AppModule {}
