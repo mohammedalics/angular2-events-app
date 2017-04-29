96836d982b0b8e0c21ce8c9f4b0bc77f291e6399
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-04-29 17:24:35 +0200

    Add create event component with navigation

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,8 @@ import { EventAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { CreateEventComponent } from './events/create-event/create-event.component'
+
 import { NavBarComponent } from './nav/navbar.component'
 import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
@@ -15,7 +17,14 @@ import { appRoutes } from './routes'
         BrowserModule, 
         RouterModule.forRoot(appRoutes)
     ], 
-    declarations: [EventAppComponent, EventsListComponent, EventThumbnailComponent, EventDetailsComponent, NavBarComponent], 
+    declarations: [
+        EventAppComponent, 
+        EventsListComponent, 
+        EventThumbnailComponent, 
+        EventDetailsComponent, 
+        CreateEventComponent, 
+        NavBarComponent
+        ], 
     providers: [EventService, ToastrService],
     bootstrap: [EventAppComponent]
 })
