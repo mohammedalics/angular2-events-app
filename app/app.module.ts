5dfd79f4b99c8867258eda4ced673ce8e4cb64b5
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-04-30 02:12:42 +0200

    Organize imports by barrels

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
+import {
+    EventsListComponent, 
+    EventThumbnailComponent, 
+    EventDetailsComponent, 
+    CreateEventComponent, 
+    EventRouterActivator, 
+    EventsListResolver, 
+    EventService
+
+} from './events/index'
 import { EventAppComponent } from './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
-import { CreateEventComponent } from './events/create-event/create-event.component'
 import { Error404Component } from './errors/404.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
-import { EventRouterActivator } from './events/event-route-activator.service'
-import { EventsListResolver } from './events/events-list-resolver.service'
-
 import { appRoutes } from './routes'
 
 @NgModule({
--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router'
-import { EventsListComponent } from './events/events-list.component'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
-import { CreateEventComponent } from './events/create-event/create-event.component'
-import { EventRouterActivator } from './events/event-route-activator.service'
-import { EventsListResolver } from './events/events-list-resolver.service'
+import {
+    EventsListComponent, 
+    EventDetailsComponent, 
+    CreateEventComponent, 
+    EventRouterActivator, 
+    EventsListResolver
+} from './events/index'
+
 import { Error404Component } from './errors/404.component'
 
 export const appRoutes:Routes = [
