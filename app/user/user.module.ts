0fa78a252a883c7abc20e5197e31f3152192e3b8
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-04-30 14:22:14 +0200

    Add basic login form with FormModule

--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 import { ProfileComponent } from './profile.component'
+import { LoginComponent } from './login.component'
 import { userRoutes } from './user.routes'
 @NgModule({
     imports: [
-        CommonModule, 
+        CommonModule,
+        FormsModule, 
         RouterModule.forChild(userRoutes)
     ], 
     declarations: [
-       ProfileComponent
+       ProfileComponent, 
+       LoginComponent
         ], 
     providers: [
        
