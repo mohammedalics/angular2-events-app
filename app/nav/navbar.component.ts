511158c131e58ca16e1126e10e69364d0f71340e
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-05-01 18:36:47 +0200

    Add searchSession service method

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
 import { AuthService } from '../user/auth.service'
+import { ISession, EventService } from '../events/shared/index'
+
 @Component({
     selector: 'nav-bar',
-    templateUrl: '/app/nav/navbar.component.html', 
+    templateUrl: '/app/nav/navbar.component.html',
     styles: [`
         .nav.navbar-nav { font-size: 15px; }
         #searchForm { margin-right: 100px; }
@@ -12,9 +14,15 @@ import { AuthService } from '../user/auth.service'
 
 })
 export class NavBarComponent {
-
-    constructor(private authService: AuthService) {
+    searchTerm: string = "";
+    foundSessions: ISession[];
+    constructor(private authService: AuthService, private eventService: EventService) {
 
     }
 
+    searchSessions(searchTerm) {
+        this.eventService.searchSessions(searchTerm).subscribe(
+            sessions => this.foundSessions = sessions)
+        console.log(this.foundSessions)
+    }
 }
