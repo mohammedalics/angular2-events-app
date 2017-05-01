d60d38e4d4dfb80219bbe01fb937ba1f859b2026
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-05-01 13:38:44 +0200

    add sort sessions buttons by name and votes

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -6,15 +6,17 @@ import { ISession } from '../shared/index'
     templateUrl: '/app/events/event-details/session-list.component.html'
 })
 export class SessionListComponent implements OnChanges {
-       
 
-    @Input() sessions:ISession[];
+
+    @Input() sessions: ISession[];
     @Input() filterBy: string;
+    @Input() sortBy: string;
     visibleSessions: ISession[] = [];
-    
+
     ngOnChanges(): void {
         if (this.sessions) {
             this.filterSessions(this.filterBy)
+            this.sortSessions(this.sortBy)
         }
     }
 
@@ -24,5 +26,11 @@ export class SessionListComponent implements OnChanges {
         } else {
             this.visibleSessions = this.sessions.filter(session => session.level.toLocaleLowerCase() === filter)
         }
+    }
+
+    sortSessions(sortBy) {
+        sortBy === 'name' ?
+            this.visibleSessions = this.visibleSessions.sort((s1: ISession, s2: ISession) => s1.name > s2.name ? 1 : (s1.name < s2.name ? -1 : 0)) :
+            this.visibleSessions = this.visibleSessions.sort((s1: ISession, s2: ISession) => s2.voters.length - s1.voters.length)
     }
 }
