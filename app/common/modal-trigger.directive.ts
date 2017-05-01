4d3bc3f9af8930e83e8650ce84f7d19cfe2ef804
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-05-01 19:43:03 +0200

    Ad ID to the modal

--- app/common/modal-trigger.directive.ts
+++ app/common/modal-trigger.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Inject, ElementRef } from '@angular/core'
+import { Directive, OnInit, Inject, ElementRef , Input} from '@angular/core'
 import { JQUERY_TOKEN } from './jquery.service'
 
 @Directive({
@@ -8,6 +8,7 @@ import { JQUERY_TOKEN } from './jquery.service'
 
 export class ModalTriggerDirective implements OnInit {
 
+    @Input('modal-trigger') modalId : string;
     private el: HTMLElement
 
     constructor(ref: ElementRef, @Inject(JQUERY_TOKEN) private $: any) {
@@ -16,6 +17,6 @@ export class ModalTriggerDirective implements OnInit {
 
     ngOnInit(): void {
         this.el.addEventListener('click', e => {
-            this.$('#simple-modal').modal({})})
+            this.$(`#${this.modalId}`).modal({})})
     }
 }
