e13cd2581ce9c744777341585b9b3ed91f206074
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-05-03 10:11:01 +0200

    Input setter for iconColor

--- app/events/event-details/upvote.component.ts
+++ app/events/event-details/upvote.component.ts
@@ -7,8 +7,7 @@ import { Component, EventEmitter, Input, Output} from '@angular/core'
         <div class="votingWeightContainer pointable" (click)="onClick()">
             <div class="well votingWeidget">
                 <div class="votingButton">
-                    <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-                    <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
+                    <i [style.color]="iconColor" class="glyphicon glyphicon-heart"></i>
                 </div>
                 <div class="badge badge-inverse votingCount">
                     <div>{{count}}</div>
@@ -20,16 +19,13 @@ import { Component, EventEmitter, Input, Output} from '@angular/core'
 export class UpvoteComponent {
 
     @Input() count: number; 
-    @Input() voted: boolean; 
+    @Input() set voted(val)  {
+        this.iconColor = val? 'red' : 'white'
+    } 
     @Output() vote = new EventEmitter(); 
-
+    iconColor: String; 
     onClick() {
-        if (!this.voted) {
-            this.count++;
-        } else {
-            this.count--;
-        }
-        this.voted = !this.voted;
+        this.vote.emit({})
     }
 
 }
