fa5f0c1e2fb508d428ba44f20315bd8a7b93d21e
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-05-04 14:35:04 +0200

    Add Karma & Jasmine

---
+++ app/test.spec.ts
@@ -0,0 +1,12 @@
+describe('first test', () => {
+    it('should be true', () => {
+        expect(true).toBe(true)
+    })
+})
+
+
+describe('second test', () => {
+    it('should be false', () => {
+        expect(false).toBe(false)
+    })
+})
