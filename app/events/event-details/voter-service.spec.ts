5cdd59383337c48b06aaadab171c0a044f93760c
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-05-05 11:33:43 +0200

    update test method

--- app/events/event-details/voter-service.spec.ts
+++ app/events/event-details/voter-service.spec.ts
@@ -8,13 +8,14 @@ describe('VoterService', () => {
 
     beforeEach(() => {
         mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post']) 
-        mockHttp.delete.and.returnValue(Observable.of(false))
         voterService = new VoterService(mockHttp)
     }); 
 
     describe('deleteVoter', () => {
         it('should remove the voter from the list of voters', () => {
             var session = {id: 6, voters: ['mohammed', 'ali']}
+            mockHttp.delete.and.returnValue(Observable.of(false))
+
             voterService.deleteVoter(3, <ISession>session, "mohammed"); 
             expect(session.voters.length).toBe(1)
             expect(session.voters[0]).toBe("ali")
