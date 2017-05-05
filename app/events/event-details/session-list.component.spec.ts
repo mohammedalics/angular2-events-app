9f256a17ffd2487bda3a8ed5ef684ebed7621dca
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-05-05 13:43:52 +0200

    add session-list component test

---
+++ app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,41 @@
+import { SessionListComponent } from './session-list.component'
+import { ISession } from './index'
+
+describe('SessionListComponent', () => {
+    let component: SessionListComponent; 
+    let mockAuthService, mockVoterService; 
+    
+    beforeEach(() => {
+        component = new SessionListComponent(mockAuthService, mockVoterService)
+
+    })
+
+    describe('ngOnChange', () => {
+
+        it('should filter the sessions correctly', () => {
+            component.sessions = <ISession[]>[
+                {name: 'session 1', level: 'intermediate'}, 
+            {name: 'session 2', level: 'beginner'}, 
+            {name: 'session 3', level: 'intermediate'}]; 
+            component.filterBy = 'intermediate'; 
+            component.sortBy = 'name'; 
+            component.eventId = 3; 
+            component.ngOnChanges();
+            expect(component.visibleSessions.length).toBe(2);
+        })
+
+
+        it('should sort the sessions correctly', () => {
+            component.sessions = <ISession[]>[
+                {name: 'session 1', level: 'intermediate'}, 
+            {name: 'session 3', level: 'beginner'}, 
+            {name: 'session 2', level: 'intermediate'}]; 
+            component.filterBy = 'all'; 
+            component.sortBy = 'name'; 
+            component.eventId = 3; 
+            component.ngOnChanges();
+            expect(component.visibleSessions.length).toBe(3);
+            expect(component.visibleSessions[2].name).toBe('session 3');
+        })
+    })
+})
