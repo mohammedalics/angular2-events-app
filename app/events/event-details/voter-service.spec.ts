73d11793715f9643c28f8115c48026692e26fcf7
Mohammed Ali <mohammed.ali.cs@gmail.com>
2017-05-05 11:18:32 +0200

    Add unit tests for voter service

---
+++ app/events/event-details/voter-service.spec.ts
@@ -0,0 +1,24 @@
+import { VoterService } from './voter.service'
+import { ISession } from '../shared/event.model'
+import { Observable } from 'rxjs/Rx'
+
+describe('VoterService', () => {
+    let voterService: VoterService;
+    let mockHttp: any
+
+    beforeEach(() => {
+        mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post']) 
+        mockHttp.delete.and.returnValue(Observable.of(false))
+        voterService = new VoterService(mockHttp)
+    }); 
+
+    describe('deleteVoter', () => {
+        it('should remove the voter from the list of voters', () => {
+            var session = {id: 6, voters: ['mohammed', 'ali']}
+            voterService.deleteVoter(3, <ISession>session, "mohammed"); 
+            expect(session.voters.length).toBe(1)
+            expect(session.voters[0]).toBe("ali")
+        })
+    })
+
+})
